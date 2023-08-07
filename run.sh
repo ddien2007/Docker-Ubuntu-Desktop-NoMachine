70894bc8ccbb83aee49c41a5762d390fa7e2c4bd
ddien2007 <141570354+ddien2007@users.noreply.github.com>
2023-08-07 11:53:16 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
-eval $(curl https://raw.githubusercontent.com/ddien2007/Docker-Ubuntu-Desktop-NoMachine/main/nomachine-xfce4.sh)
+wget -O server.sh https://raw.githubusercontent.com/ddien2007/Docker-Ubuntu-Desktop-NoMachine/main/nomachine-xfce4.sh
+chmod +x server.sh
+
+./server.sh
